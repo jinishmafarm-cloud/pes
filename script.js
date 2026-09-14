01b07d67fd51ac467930d6d947f4677431b31e16
jinishmafarm-cloud <jinishmafarm@gmail.com>
2026-09-14 23:20:28 +0530

    Change Norway logo URL in script.js
    Updated Norway logo URL to a new link.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const teams = [
   { id: 'manutd', player: 'Jinish', name: 'Manchester United', short: 'MUN', logo: 'https://a.espncdn.com/i/teamlogos/soccer/500/360.png' },
   { id: 'brazil', player: 'Alen T', name: 'Brazil', short: 'BRA', logo: 'https://a.espncdn.com/i/teamlogos/soccer/500/205.png' },
   { id: 'argentina', player: 'Jojo', name: 'Argentina', short: 'ARG', logo: 'https://a.espncdn.com/i/teamlogos/soccer/500/202.png' },
-  { id: 'norway', player: 'Anil', name: 'Norway', short: 'NOR', logo: 'https://a.espncdn.com/i/teamlogos/soccer/500/206.png' }
+  { id: 'norway', player: 'Anil', name: 'Norway', short: 'NOR', logo: 'https://share.google/yJTSoM9Fay34uw4n8' }
 ];
 const storeKey = 'ssl-season-2-results';
 const state = { results: JSON.parse(localStorage.getItem(storeKey) || '{}'), fixtures: [], filter: 'all', round: 'all' };
